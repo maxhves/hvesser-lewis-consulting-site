91ed698e38769c0e5c7e892f597f427d78fedcac
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-12-28 22:29:03 -0800

    Began setting up basic project structure.

---
+++ src/app/components/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div className="w-full bg-red-500 p-5 flex justify-center">
+      <div className="bg-blue-500 w-full lg:w-11/12">
+        <h1>MHL</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/app/sections/introduction/introduction.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Introduction() {
+  return (
+    <div className="flex justify-center p-5">
+      <div className="w-full lg:w-10/12 bg-yellow-500">
+        <h1>Hello, my name is Max!</h1>
+        <p>This is where I am going to write a little about myself.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Introduction;
