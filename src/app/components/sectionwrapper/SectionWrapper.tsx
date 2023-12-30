b0edad5f5990f3e2057a4f9816bc43b52aa74561
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-12-29 20:21:24 -0800

    Added the ability to change SectionWrapper background.

--- src/app/components/sectionwrapper/SectionWrapper.tsx
+++ src/app/components/sectionwrapper/SectionWrapper.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function SectionWrapper({children}: { children: React.ReactNode }) {
+type SectionType = 'primary' | 'secondary';
+
+function SectionWrapper(
+  {children, type = 'primary'}: { children: React.ReactNode, type?: SectionType }
+) {
+  let background: string = type === 'primary' ? "bg-old-lace" : "bg-aths-special"
+
   return (
-    <div className="flex justify-center">
+    <div className={`flex justify-center ${background}`}>
       <div className="w-full lg:w-9/12 py-10 lg:py-20 px-5">
         {children}
       </div>
