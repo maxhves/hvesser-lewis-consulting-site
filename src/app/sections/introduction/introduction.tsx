b6046637476443f060f1ef96b9325b1542fd3ab8
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-01-01 16:59:08 -0800

    Adjusted text size in introduction.

--- src/app/sections/introduction/introduction.tsx
+++ src/app/sections/introduction/introduction.tsx
@@ -6,10 +6,10 @@ function Introduction() {
     <SectionWrapper>
       <div className="flex flex-col lg:flex-row h-full space-y-12 space-x-0 lg:space-y-0 lg:space-x-24 items-center">
         {/* Introduction text */}
-        <div className="w-full lg:w-2/3 items-center lg:items-start text-center lg:text-start space-y-3">
-          <p className="text-2xl text-cutty-sark">Hi, I&apos;m Max 👋</p>
-          <p className="text-3xl text-cutty-sark font-semibold">I love finding solutions</p>
-          <p className="text-2xl text-cutty-sark">
+        <div className="w-full lg:w-2/3 items-center lg:items-start text-center lg:text-start text-cutty-sark space-y-2">
+          <p>Hi, I&apos;m Maximilian 👋</p>
+          <p className="text-2xl font-semibold">I love finding solutions</p>
+          <p>
             I am an experienced Software Engineer and Android Developer, currently working at
             <span className="text-orange-roughy font-medium"> Vipps</span> creating simplicity in peoples&apos; financial lives.
           </p>
