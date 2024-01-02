39c7c9a2aa6c2646a94b3356f634e4f3576d2486
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-01-01 17:06:22 -0800

    Updated text sizing in navbar/

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -7,11 +7,11 @@ function Navbar() {
       <div className="flex justify-between w-full lg:w-10/12">
         {/* Logo */}
         <div>
-          <p className="text-tiber font-bold">MHL</p>
+          <p className="text-tiber font-bold text-lg">MHL</p>
         </div>
 
         {/* Links */}
-        <div className="hidden lg:block space-x-8 text-tiber">
+        <div className="hidden lg:block space-x-8 text-tiber text-sm">
           <Link href="/" className="hover:text-jungle-green"><span className="text-jungle-green">01.</span> About</Link>
           <Link href="/" className="hover:text-jungle-green"><span className="text-jungle-green">02.</span> Experience</Link>
           <Link href="/" className="hover:text-jungle-green"><span className="text-jungle-green">03.</span> Projects</Link>
