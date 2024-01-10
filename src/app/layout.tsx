8b3ed5cc7513bb3d22067b80a63bc161bfc79d72
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-01-10 12:12:57 -0800

    Removed extraneous icon files.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Providers } from './providers'
 import {IBM_Plex_Mono} from "next/font/google";
 import './globals.css'
-import Head from "next/head";
 
 const ibmPlexMono = IBM_Plex_Mono({weight: ['100', '200', '300', '400', '500', '600', '700'], subsets: ['latin']})
 
@@ -20,22 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
-    <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-      <link rel="manifest" href="/site.webmanifest"/>
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-      <meta name="msapplication-TileColor" content="#da532c"/>
-    </Head>
-    <body id="home"
-          className={`${ibmPlexMono.className} [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] bg-old-lace dark:bg-tiber`}>
-    <Providers>
-      {children}
-    </Providers>
-    <SpeedInsights/>
-    <Analytics/>
-    </body>
+      <body id="home" className={`${ibmPlexMono.className} [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] bg-old-lace dark:bg-tiber`}>
+        <Providers>
+          {children}
+        </Providers>
+        <SpeedInsights/>
+        <Analytics/>
+      </body>
     </html>
   )
 }
