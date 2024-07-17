2bf3d85601bc5ce3e271e0ec68ab9534947d26cb
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-07-17 17:13:09 +1200

    Updated secondary project components.

--- src/app/home/components/projects/projects-section.tsx
+++ src/app/home/components/projects/projects-section.tsx
@@ -13,7 +13,7 @@ import {
   PrimaryProjectVisitButton
 } from "@/app/home/components/projects/primary-projects-list";
 import {
-  SecondaryProjectDescription,
+  SecondaryProjectDescription, SecondaryProjectHeader,
   SecondaryProjectLink,
   SecondaryProjectListItem,
   SecondaryProjectsList,
@@ -22,6 +22,7 @@ import {
   SecondaryProjectTitle
 } from "@/app/home/components/projects/secondary-projects-list";
 import {PROJECTS_NAV_SECTION} from "@/app/home/data/navigation/home-navigation-section";
+import {Technology} from "@/types/project/technology";
 
 //region Model
 
@@ -30,21 +31,21 @@ const primaryProjects: Project[] = [
     title: "Temperature Check",
     description: "Temperature reading interpretation whereby users are able to enter their body temperature reading and understand if it is normal, or considered a fever. Measurement settings are available for accurate interpretation.",
     href: "https://www.temperature-check.com",
-    technologies: ["Typescript", "ReactJS", "NextJS", "TailwindCSS"],
+    technologies: [Technology.TYPESCRIPT, Technology.REACTJS, Technology.NEXTJS, Technology.TAILWINDCSS],
     imageSource: "/images/projects/temperature-check.png"
   },
   {
     title: "Crime Connoisseur",
     description: "A true-crime podcast player and discovery platform. This site allows users to easily find, listen to and rate podcasts/episodes in the true-crime category, whilst highlighting new and updated podcasts.",
     href: "https://www.crimeconnoisseur.com",
-    technologies: ["Typescript", "ReactJS", "NextJS", "TailwindCSS", "Supabase"],
+    technologies: [Technology.TYPESCRIPT, Technology.REACTJS, Technology.NEXTJS, Technology.TAILWINDCSS, Technology.SUPABASE],
     imageSource: "/images/projects/crime-connoisseur.png"
   },
   {
     title: "Visa Match",
     description: "Informational guides to working abroad, informing users about what visa options are potentially available to them.Each guide provides an overview of the requirements, alongside positive and negative points.",
     href: "https://www.visamatch.co",
-    technologies: ["Typescript", "ReactJS", "NextJS", "TailwindCSS", "Supabase"],
+    technologies: [Technology.TYPESCRIPT, Technology.REACTJS, Technology.NEXTJS, Technology.TAILWINDCSS, Technology.SUPABASE],
     imageSource: "/images/projects/visa-match.png"
   }
 ]
@@ -54,13 +55,13 @@ const secondaryProjects: Project[] = [
     title: "Swap",
     description: "A native mobile currency converter helping users to convert between the most popular global currencies.",
     href: "https://github.com/maxhves?tab=repositories&q=swap",
-    technologies: ["Android", "iOS", "Kotlin", "Swift"]
+    technologies: [Technology.ANDROID, Technology.IOS, Technology.KOTLIN, Technology.SWIFT]
   },
   {
     title: "Showroom",
     description: "An image gallery library built for Android, used for creating an immersive image showcase.",
     href: "https://github.com/maxhves/showroom",
-    technologies: ["Android", "Kotlin"]
+    technologies: [Technology.ANDROID, Technology.KOTLIN]
   }
 ]
 
@@ -73,15 +74,14 @@ export default function ProjectsSection() {
     <section id={PROJECTS_NAV_SECTION.id} className="bg-stone-50 scroll-mt-14">
       <ContentContainer className="py-16">
         <SectionHeader>
-          Side Projects
+          Projects
         </SectionHeader>
 
         <ProjectsDescription>
-          I am constantly working to improve myself and learn new things. Pursuing and developing my own ideas gives me
-          the motivation to learn in an efficient and focused way.
-          {`\n\n`}
-          Through my own projects, I have gained a great deal of experience and knowledge over a range of different
-          topics and technology stacks, take a look below at some of these projects.
+          The projects you see listed here are just some of the ideas that I have been developing and working on during
+          my self-employment. I am passionate about working on projects that personally motivate me and I am passionate
+          about developing my own ideas and that is why I gave myself the opportunity to work on something that enables
+          me to do exactly that.
         </ProjectsDescription>
 
         <PrimaryProjectsList className="mt-16">
@@ -99,9 +99,9 @@ export default function ProjectsSection() {
               </PrimaryProjectDescription>
               <PrimaryProjectFooter>
                 <PrimaryProjectTechnologiesList>
-                  {project.technologies.map((technology: string) => (
-                    <PrimaryProjectTechnologyListItem key={technology}>
-                      {technology}
+                  {project.technologies.map((technology: Technology) => (
+                    <PrimaryProjectTechnologyListItem key={technology.toString()}>
+                      {technology.toString()}
                     </PrimaryProjectTechnologyListItem>
                   ))}
                 </PrimaryProjectTechnologiesList>
@@ -114,25 +114,29 @@ export default function ProjectsSection() {
         </PrimaryProjectsList>
 
         <SectionSubheader className="mt-16">
-          See also
+          Open source
         </SectionSubheader>
 
         <SecondaryProjectsList className="mt-4">
           {secondaryProjects.map((project: Project) => (
             <SecondaryProjectListItem key={project.title}>
-              <SecondaryProjectTitle>
-                {project.title}
-              </SecondaryProjectTitle>
+              <SecondaryProjectHeader>
+                <SecondaryProjectTitle>
+                  {project.title}
+                </SecondaryProjectTitle>
+                <SecondaryProjectLink href={project.href} />
+              </SecondaryProjectHeader>
+
               <SecondaryProjectDescription>
                 {project.description}
               </SecondaryProjectDescription>
-              <SecondaryProjectLink href={project.href}>
-                See project on Github
-              </SecondaryProjectLink>
               <SecondaryProjectTechnologiesList>
-                {project.technologies.map((technology: string) => (
-                  <SecondaryProjectTechnologyListItem key={technology}>
-                    {technology}
+                {project.technologies.map((technology: Technology) => (
+                  <SecondaryProjectTechnologyListItem
+                    key={technology.toString()}
+                    color={technology.color}
+                  >
+                    {technology.toString()}
                   </SecondaryProjectTechnologyListItem>
                 ))}
               </SecondaryProjectTechnologiesList>
@@ -150,7 +154,7 @@ export default function ProjectsSection() {
 
 function ProjectsDescription({children}: { children: React.ReactNode }) {
   return (
-    <p className="mt-4 text-sm text-neutral-400 whitespace-pre-wrap">
+    <p className="mt-4 text-sm font-normal text-stone-700 whitespace-pre-wrap">
       {children}
     </p>
   )
