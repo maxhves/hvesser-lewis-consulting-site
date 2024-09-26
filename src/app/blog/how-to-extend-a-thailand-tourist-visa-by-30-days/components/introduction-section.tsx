467fd7b74743fd0b00a361ae98acbceff9829c1a
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-09-26 21:36:08 +0700

    Setup introduction section components.

---
+++ src/app/blog/how-to-extend-a-thailand-tourist-visa-by-30-days/components/introduction-section.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {clsx} from "clsx";
+import {karla} from "@/app/fonts";
+
+//region Entry
+
+export default function IntroductionSection({children}: { children: React.ReactNode }) {
+  return (
+    <section>
+      {children}
+    </section>
+  )
+}
+
+//endregion
+
+//region Texts
+
+export function IntroductionPreTitle({children}: { children: React.ReactNode }) {
+  return (
+    <p className={clsx(karla.className, "font-semibold text-base leading-7 text-orange-600")}>
+      {children}
+    </p>
+  )
+}
+
+export function IntroductionTitle({children}: { children: React.ReactNode }) {
+  return (
+    <h1 className={clsx(karla.className, "mt-2 text-2xl font-bold tracking-tight text-stone-900 sm:text-3xl")}>
+      {children}
+    </h1>
+  )
+}
+
+export function IntroductionPreface({children}: { children: React.ReactNode }) {
+  return (
+    <p className="mt-6 text-xl leading-8 text-stone-700">
+      {children}
+    </p>
+  )
+}
+
+export function IntroductionBody({children}: { children: React.ReactNode }) {
+  return (
+    <p className="mt-8 text-base leading-7 text-gray-700">
+      {children}
+    </p>
+  )
+}
+
+//endregion
--- src/app/blog/how-to-extend-a-thailand-tourist-visa-by-30-days/page.tsx
+++ src/app/blog/how-to-extend-a-thailand-tourist-visa-by-30-days/page.tsx
@@ -1,9 +1,40 @@
+import ContentContainer from "@/components/ui/content-container";
+import IntroductionSection
+  , {
+  IntroductionBody,
+  IntroductionPreface,
+  IntroductionPreTitle,
+  IntroductionTitle
+} from "@/app/blog/how-to-extend-a-thailand-tourist-visa-by-30-days/components/introduction-section";
+
 //region Entry
 
 export default function HowToExtendAThailandTouristVisaBy30Days() {
   return (
     <main className="flex-grow">
-      <div>Article goes here...</div>
+      <ContentContainer className="py-32">
+        <article>
+          <IntroductionSection>
+            <IntroductionPreTitle>
+              A guide on
+            </IntroductionPreTitle>
+            <IntroductionTitle>
+              How to extend a Thailand tourist visa by 30 days
+            </IntroductionTitle>
+            <IntroductionPreface>
+              If, like me, you&apos;ve recently taken advantage of Thailand&apos;s 60-day visa exemption or 60-day Visa
+              on Arrival, you might now be wondering how to extend your stay by an additional 30 days. Official
+              information on the process can seem a bit scarce and confusing, so I’ve put together this guide to clarify
+              how you can extend your visa, based on my own recent experience.
+            </IntroductionPreface>
+            <IntroductionBody>
+              I&apos;d like to start by noting that immigration offices may vary slightly depending on their location
+              and specific procedures. So, where it&apos;s relevant, the information I&apos;m sharing here is based on
+              my personal experience of extending my 60-day visa exemption in Bangkok.
+            </IntroductionBody>
+          </IntroductionSection>
+        </article>
+      </ContentContainer>
     </main>
   )
 }
