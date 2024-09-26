683cd3611e885e5a1ddadfb5ae0b80afcb4a4929
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-09-26 15:17:42 +0700

    Setup blog header components.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,21 @@
 //region Entry
 
+import ContentContainer from "@/components/ui/content-container";
+import Header, {Heading, Subheading} from "@/app/blog/components/header";
+
 export default function Blog() {
   return (
     <main className="flex-grow">
-      <div>Blog pages goes here...</div>
+      <ContentContainer className="py-32">
+        <Header>
+          <Heading>
+            The Blog
+          </Heading>
+          <Subheading>
+            A place where I share interesting and noteworthy self-written articles
+          </Subheading>
+        </Header>
+      </ContentContainer>
     </main>
   )
 }
