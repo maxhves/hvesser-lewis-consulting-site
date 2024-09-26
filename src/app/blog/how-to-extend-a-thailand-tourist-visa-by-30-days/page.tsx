62dc861f08634268fbd5936fdd580a0c745b5c1f
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-09-26 14:27:33 +0700

    Setup basic folder structure.

---
+++ src/app/blog/how-to-extend-a-thailand-tourist-visa-by-30-days/page.tsx
@@ -0,0 +1,11 @@
+//region Entry
+
+export default function HowToExtendAThailandTouristVisaBy30Days() {
+  return (
+    <main className="flex-grow">
+      <div>Article goes here...</div>
+    </main>
+  )
+}
+
+//endregion
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,11 @@
+//region Entry
+
+export default function Blog() {
+  return (
+    <main className="flex-grow">
+      <div>Blog pages goes here...</div>
+    </main>
+  )
+}
+
+//endregion
