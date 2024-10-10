b33029b81bbbd67b865e9b309a2341af7b5e689d
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-10-10 09:49:06 +0700

    Updated favicon resources.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import HomeFooter from "@/app/home/components/home-footer";
 //region Metadata
 
 export const metadata: Metadata = {
-  title: 'Maximilian Hvesser-Lewis',
-  description: 'Full Stack Developer • Web Developer • Development Agency',
+  title: 'Hvesser-Lewis Consulting',
+  description: 'Full Stack Developer • Mobile Developer • Web Developer',
 }
 
 //endregion
@@ -21,18 +21,19 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>Hvesser-Lewis Consulting</title>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body id="home" className={clsx(inter.className, "bg-slate-50 h-dvh flex flex-col antialiased")}>
-        {/* Navigation bar */}
         <HomeNavigationBar />
-
-        {/* Content */}
         {children}
-
-        {/* Footer */}
         <HomeFooter />
-
-        {/* Analytics */}
-        <Analytics/>
+        <Analytics />
       </body>
     </html>
   )
