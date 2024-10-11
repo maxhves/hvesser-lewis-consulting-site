9693f352bf8b8d2b37c5efa42eb145e5ff00be8b
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-10-11 16:40:34 +0700

    CHanged max width of content container to 4xl.

--- src/components/ui/content-container.tsx
+++ src/components/ui/content-container.tsx
@@ -6,7 +6,7 @@ import {clsx} from "clsx";
 export default function ContentContainer({className, children}: { className?: string, children: React.ReactNode }) {
   return (
     <div className={clsx(className, "w-full flex justify-center px-4 sm:px-8")}>
-      <div className="flex flex-col w-full max-w-3xl">
+      <div className="flex flex-col w-full max-w-4xl">
         {children}
       </div>
     </div>
