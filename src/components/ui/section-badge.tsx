9d80001f8ee376701b48152833281e8c7debda60
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-10-11 16:55:40 +0700

    Updated section badge to match latest desigs.

--- src/components/ui/section-badge.tsx
+++ src/components/ui/section-badge.tsx
@@ -5,11 +5,9 @@ import {clsx} from "clsx";
 
 export default function SectionBadge({className, children}: { className?: string, children: React.ReactNode }) {
   return (
-    <div className={clsx(className, "py-1 px-2 rounded-full bg-blue-100 w-fit")}>
-      <p className="text-xs font-medium text-blue-700">
-        {children}
-      </p>
-    </div>
+    <p className={clsx(className, "uppercase tracking-wide font-extrabold text-xs text-emerald-600")}>
+      {children}
+    </p>
   )
 }
 
