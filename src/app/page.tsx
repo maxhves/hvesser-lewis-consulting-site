44f8ee62cadcef308525e664dca31b1d5875c105
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2024-10-11 16:41:42 +0700

    Removed now redundant dividers.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import AboutSection from "@/app/home/components/about/about-section";
 import IntroductionSection from "@/app/home/components/introduction/introduction-section";
 import ProjectsSection from "@/app/home/components/projects/projects-section";
 import ContactSection from "@/app/home/components/contact/contact-section";
-import {Divider} from "@/components/ui/divider";
 import ServicesSection from "@/app/home/components/services/services-section";
 
 //region Entry
@@ -11,13 +10,9 @@ export default function Home() {
   return (
     <main className="flex-grow">
       <IntroductionSection />
-      <Divider />
       <AboutSection />
-      <Divider />
       <ServicesSection />
-      <Divider />
       <ProjectsSection />
-      <Divider />
       <ContactSection />
     </main>
   )
