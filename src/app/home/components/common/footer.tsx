c03d6b36e8ea417a4549d9c31304b7e188580a30
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-02-27 20:35:33 +1300

    Finished updating footer component.

--- src/app/home/components/common/footer.tsx
+++ src/app/home/components/common/footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { ContentContainerNew } from '@/components/ui/content-container'
+import { IconBrandGithub, IconBrandLinkedin, IconClipboardText, IconMail } from '@tabler/icons-react'
+import { Link } from '@/components/ui/link'
 
 //region Social Links
 
@@ -7,19 +9,22 @@ const socialLinks = [
   {
     label: 'Github',
     href: 'https://github.com/maxhves',
-    icon:
+    icon: IconBrandGithub,
   },
   {
     label: 'LinkedIn',
     href: 'https://www.linkedin.com/in/maxhves/',
+    icon: IconBrandLinkedin,
   },
   {
     label: 'Email',
     href: 'mailto:max@hvesserlewis.com',
+    icon: IconMail,
   },
   {
     label: 'Resume',
     href: 'https://hvesserlewis.com/resume',
+    icon: IconClipboardText,
   },
 ]
 
@@ -29,8 +34,9 @@ export default function Footer() {
   return (
     <footer className="border-t border-stone-200">
       <ContentContainerNew className="py-8">
-        <div className="flex h-8 items-center border-y border-stone-200 px-6">
+        <div className="flex flex-col items-center justify-normal gap-5 border-y border-stone-200 md:flex-row md:justify-between">
           <Name />
+          <SocialLinks />
         </div>
       </ContentContainerNew>
     </footer>
@@ -41,8 +47,35 @@ export default function Footer() {
 
 function Name() {
   return (
-    <div className="flex h-full items-center border-x border-stone-200 text-sm text-stone-500">
-      Maximilian Hvesser-Lewis
+    <div className="mx-5 flex w-full justify-center border-b border-stone-200 md:w-fit md:border-none">
+      <div className="flex h-8 items-center border-x border-stone-200 text-sm text-stone-500">
+        Maximilian Hvesser-Lewis
+      </div>
+    </div>
+  )
+}
+
+//endregion
+
+//region Social Links
+
+function SocialLinks() {
+  return (
+    <div className="mx-5 flex w-full justify-center border-t border-stone-200 md:w-fit md:border-none">
+      <div className="flex w-fit items-center gap-1 border-x border-stone-200">
+        {socialLinks.map((link) => (
+          <Link
+            className="flex h-8 items-center gap-1 px-3 text-sm text-stone-500 hover:text-stone-700"
+            key={link.label}
+            href={link.href}
+            rel="noreferrer"
+            target="_blank"
+          >
+            <link.icon className="size-5 md:size-4" />
+            <span className="hidden md:flex">{link.label}</span>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
