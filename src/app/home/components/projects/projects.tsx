fadac0ca2ad755822a78996e26354241099fe2b4
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-02-28 15:27:22 +1300

    Added basic projects section.

---
+++ src/app/home/components/projects/projects.tsx
@@ -0,0 +1,13 @@
+import SectionHeading from '@/app/home/components/common/section-heading'
+import { ContentContainerNew } from '@/components/ui/content-container'
+
+export default function Projects() {
+  return (
+    <section>
+      <ContentContainerNew>
+        <SectionHeading>A number of projects have kept me busy recently</SectionHeading>
+        <div className="h-32 w-full" />
+      </ContentContainerNew>
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Introduction from '@/app/home/components/introduction/introduction'
 import { Expertise } from '@/app/home/components/expertise/expertise'
+import Projects from '@/app/home/components/projects/projects'
 
 export default function Home() {
   return (
     <main className="flex-grow">
       <Introduction />
       <Expertise />
+      <Projects />
       {/*<AboutSection />*/}
       {/*<ServicesSection />*/}
       {/*<PortfolioSection />*/}
