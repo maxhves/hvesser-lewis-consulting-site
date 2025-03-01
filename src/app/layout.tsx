d2e409a5aff210ad640eb3a309a4c20810cae66f
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-01 15:27:24 +1300

    Setup basic theme switch with next-themes.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,20 @@ import './globals.css'
 import { clsx } from 'clsx'
 import HomeNavigationBar from '@/app/home/components/navigation/home-navigation-bar'
 import Footer from '@/app/home/components/common/footer'
+import { ThemeProvider } from 'next-themes'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body id="home" className={clsx(manrope.className, 'flex h-dvh flex-col bg-stone-100 antialiased')}>
-        <HomeNavigationBar />
-        {children}
-        <Footer />
+    <html lang="en" suppressHydrationWarning={true}>
+      <body
+        id="home"
+        className={clsx(manrope.className, 'flex h-dvh flex-col bg-stone-100 antialiased dark:bg-stone-900')}
+      >
+        <ThemeProvider attribute="class" enableSystem={false}>
+          <HomeNavigationBar />
+          {children}
+          <Footer />
+        </ThemeProvider>
         <Analytics />
       </body>
     </html>
